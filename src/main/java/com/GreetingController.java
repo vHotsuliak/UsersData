02e3bbfee1f168f81bc1f9cb1dad5628b81d15af
vHotsuliak <vasyl.hotsuliak@gmail.com>
2018-10-03 23:00:14 +0300

    greeting.html

--- src/main/java/com/GreetingController.java
+++ src/main/java/com/GreetingController.java
@@ -3,13 +3,20 @@ package com;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    public String greetingGet(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
     }
+
+    @PostMapping("/greeting")
+    public String greetingPost(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        return null;
+    }
+
 }
