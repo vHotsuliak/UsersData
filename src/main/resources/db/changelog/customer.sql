d28939ce6de2a254dbedc8f7e67af6a6f26fceb7
vHotsuliak <42220970+vHotsuliak@users.noreply.github.com>
2018-10-03 14:55:30 +0700

    Update customer.sql

--- src/main/resources/db/changelog/customer.sql
+++ src/main/resources/db/changelog/customer.sql
@@ -1,7 +1,7 @@
+--liquibase formatted sql
 
-
-
-CREATE TABLE `customer` (
+--changeset Customer:1
+CREATE TABLE `Customer` (
   `customerid` int(11) NOT NULL,
   `employeeid` int(11) NOT NULL,
   `name` varchar(45) DEFAULT NULL,
